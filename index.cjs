3ec8e91dbadb64d94e8d4a45ef0a7da7376dd92c
Talut Salako <salakotalut@gmail.com>
2024-01-11 15:46:49 +0100

    first commit

---
+++ index.cjs
@@ -0,0 +1,17 @@
+const http = require('node:http')
+const user = { firstName: 'Talut', lastName: 'Salako' }
+const server = http.createServer()
+const port = 8900
+server.on('request', (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.end(`My name is ${user.firstName} ${user.lastName}`)
+  } else {
+    res.statusCode = 404
+    res.end('Error: Invalid route.')
+  }
+})
+
+server.listen(port, () => {
+  console.log(`server sucessfully started on port: ${port}`)
+})
