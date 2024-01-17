1e55280a9eac17559a1a1468cd925d0f80e8e7e8
Talut Salako <salakotalut@gmail.com>
2024-01-17 18:36:11 +0100

    rename .cjs => .js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 8900
 server.on('request', (req, res) => {
   if (req.url === '/') {
     res.statusCode = 200
+    res.writeHead(200, { 'Content-type': 'text/html' })
     res.end(`My name is ${user.firstName} ${user.lastName}`)
   } else {
     res.statusCode = 404
