1f56837b9da9291607424be5b81786facbc326c1
Talut Salako <salakotalut@gmail.com>
2024-03-20 11:09:14 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const http = require('node:http')
-const user = { firstName: 'Talut', lastName: 'Salako' }
-const server = http.createServer()
-const port = 8900
-server.on('request', (req, res) => {
-  if (req.url === '/') {
-    res.statusCode = 200
-    res.writeHead(200, { 'Content-type': 'text/html' })
-    res.end(`My name is ${user.firstName} ${user.lastName}`)
+const http = require("node:http");
+const user = { firstName: "Talut", lastName: "Salako" };
+const server = http.createServer();
+const hostname = "127.0.0.1:";
+const port = 8900;
+server.on("request", (req, res) => {
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`My name is ${user.firstName} ${user.lastName}`);
   } else {
-    res.statusCode = 404
-    res.end('Error: Invalid route.')
+    res.statusCode = 404;
+    res.end("Error: Invalid route.");
   }
-})
+});
 
 server.listen(port, () => {
-  console.log(`server sucessfully started on port: ${port}`)
-  console.log(`click to open http://localhost:${port}`)
-})
+  console.log(`server sucessfully started on port: ${port}`);
+  console.log(`click to open http://localhost:${port}`);
+});
